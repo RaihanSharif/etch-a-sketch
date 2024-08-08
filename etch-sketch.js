1a8581998ac49518d3fca225ac08447fbad1d0cc
Raihan Sharif <raihansharif92@gmail.com>
2024-08-08 18:24:21 +0100

    basic filling of cells

--- etch-sketch.js
+++ etch-sketch.js
@@ -8,8 +8,23 @@ for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         let innerDiv = document.createElement("div");
         innerDiv.className = "inner";
-        innerDiv.id = `inner-${i}-${j}`;
+        innerDiv.id = j;
         outerDiv.appendChild(innerDiv);
     }
 }
 
+/*
+add listener for mouseover or mousedown at the container level
+then use the target property to color each of the inner divs 
+*/
+
+container.addEventListener('mouseover', (event) => {
+    const target = event.target;
+    // const targetID = `#${target.id}`;
+    // const elem =
+    console.log(target);
+    if (target.className === "inner") {   // hmmm??? TODO: 
+        target.style.backgroundColor = "black";
+    }
+
+});
