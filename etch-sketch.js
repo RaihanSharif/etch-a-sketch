801523b31d0ab8368cf1e0fdf31e1b4d95c67c7d
Raihan Sharif <raihansharif92@gmail.com>
2024-08-09 12:59:11 +0100

    Add buttons to resize and clear grid
    Resize button clears all elems of the grid and redraws a new grid
    clear does the same but without changing size

--- etch-sketch.js
+++ etch-sketch.js
@@ -1,24 +1,27 @@
 const container = document.querySelector("#grid-container");
 
-function drawGrid(num) {
-    for (let i = 0; i < num; i++) {
+let gridSize = 16;
+
+function drawGrid(gridSize) {
+    container.replaceChildren();
+    for (let i = 0; i < gridSize; i++) {
         let outerDiv = document.createElement("div");
         container.appendChild(outerDiv);
         outerDiv.className = "outer";
         outerDiv.id = `outer-${i}`;
-        for (let j = 0; j < num; j++) {
+        for (let j = 0; j < gridSize; j++) {
             let innerDiv = document.createElement("div");
             innerDiv.className = "inner";
-            innerDiv.id = j;
-            innerDiv.style.height = (Math.floor(960/num)).toString()+"px";
-            innerDiv.style.width = (Math.floor(960/num)).toString()+"px";
+            innerDiv.id = i.toString()+ "-" + j.toString();
+            innerDiv.style.height = (Math.floor(960/gridSize)).toString()+"px";
+            innerDiv.style.width = (Math.floor(960/gridSize)).toString()+"px";
 
             outerDiv.appendChild(innerDiv);
         }
     }
 }
 
-drawGrid(32);
+drawGrid(gridSize);
 
 let isMouseDown = false;
 container.addEventListener("mousedown", () => isMouseDown = true );
@@ -36,3 +39,18 @@ container.addEventListener('mouseover', (event) => {
 
 });
 
+const newGridBtn = document.querySelector("#btn-new-grid");
+
+newGridBtn.addEventListener('click', () => {
+    const size = parseInt(prompt("Enter a new grid size, from 1 to 64 (inclusive) for a new grid"));
+    
+    gridSize = size;
+    drawGrid(gridSize);
+})
+
+const clearGridBtn = document.querySelector("#btn-clear-grid");
+
+const cells = document.querySelectorAll(".inner");
+clearGridBtn.addEventListener('click', () => {
+    drawGrid(gridSize);
+});
