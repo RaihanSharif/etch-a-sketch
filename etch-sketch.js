9141192c324dfbce3e88a18ea441f1a0fbb653ea
Raihan Sharif <raihansharif92@gmail.com>
2024-08-09 11:31:52 +0100

    turn grid draw into a function

--- etch-sketch.js
+++ etch-sketch.js
@@ -1,27 +1,22 @@
 const container = document.querySelector("#grid-container");
 
-for (let i = 0; i < 16; i++) {
-    let outerDiv = document.createElement("div");
-    container.appendChild(outerDiv);
-    outerDiv.className = "outer";
-    outerDiv.id = `outer-${i}`;
-    for (let j = 0; j < 16; j++) {
-        let innerDiv = document.createElement("div");
-        innerDiv.className = "inner";
-        innerDiv.id = j;
-        outerDiv.appendChild(innerDiv);
+function drawGrid(num) {
+    for (let i = 0; i < num; i++) {
+        let outerDiv = document.createElement("div");
+        container.appendChild(outerDiv);
+        outerDiv.className = "outer";
+        outerDiv.id = `outer-${i}`;
+        for (let j = 0; j < num; j++) {
+            let innerDiv = document.createElement("div");
+            innerDiv.className = "inner";
+            innerDiv.id = j;
+            outerDiv.appendChild(innerDiv);
+        }
     }
 }
 
-/*
-add listener for mouseover or mousedown at the container level
-then use the target property to color each of the inner divs 
-*/
+drawGrid(16);
 
-/*
-this block is to ensure that cells of the etch-a-sketch grid is
-only filled when mouse is down.
-*/
 let isMouseDown = false;
 container.addEventListener("mousedown", () => isMouseDown = true );
 container.addEventListener("mouseup", () => isMouseDown = false);
